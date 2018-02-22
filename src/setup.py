4248a69f19527712cbb1d72f004ede81428d13de
Emlyn O'Regan <emlynoregan@gmail.com>
2018-02-22 15:24:57 +1030

    proper new version number

--- src/setup.py
+++ src/setup.py
@@ -8,7 +8,7 @@ except ImportError:
 
 dist = setup(
     name='im_util',
-    version='0.0.1dev1',
+    version='0.1.0',
     description='Utilities for the im package library; used by most of them',
     author='Emlyn O\'Regan',
     author_email='emlynoregan@gmail.com',
